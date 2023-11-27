fd9689d6ec2638a16988409173a638e7201afede
Premalsinh <88768581+Premalsinh@users.noreply.github.com>
2023-11-27 18:02:23 +0530

    Create real.py

---
+++ real.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np
+
+# Function to simulate streaming data
+def generate_data():
+    while True:
+        # Simulate streaming data (replace this with your actual data source)
+        data = pd.DataFrame({
+            'Timestamp': [pd.Timestamp.now()],
+            'Value': [np.random.rand()]
+        })
+
+        # Display the streaming data in the Streamlit app
+        st.table(data)
+
+        # Sleep to simulate real-time updates
+        time.sleep(1)
+
+# Main Streamlit app
+def main():
+    st.title("Real-Time Streaming in Streamlit")
+
+    # Start streaming data
+    generate_data()
+
+if __name__ == "__main__":
+    main()
