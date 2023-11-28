37545043c961509355607fcacca467d5fcb0dc13
Premalsinh <88768581+Premalsinh@users.noreply.github.com>
2023-11-28 15:18:42 +0530

    Update real.py

--- real.py
+++ real.py
@@ -13,7 +13,7 @@ def generate_data():
         })
 
         # Display the streaming data in the Streamlit app
-         st.add_rows(data)
+        st.add_rows(data)
 
         # Sleep to simulate real-time updates
         time.sleep(10)
